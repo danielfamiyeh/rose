39f330693a0bed93d2ddf0c89fbf1fc27cd7aa16
Daniel Famiyeh <daniel@podaris.com>
2023-06-01 23:04:15 +0100

    Add docstrings

--- src/sequence/DNA.ts
+++ src/sequence/DNA.ts
@@ -3,10 +3,17 @@ import { Sequence } from './Sequence';
 const bases = ['A', 'C', 'G', 'T'];
 
 export class DNA extends Sequence {
+  /**
+   * Creates a new DNA object
+   * @param {string} data DNA sequence string
+   */
   constructor(data: string) {
     super(data.toLocaleUpperCase(), 'dna');
   }
 
+  /**
+   * Returns array of nucleotide counts in the order A C G T
+   */
   get nucleoCount() {
     return bases.map(
       (base) => (this.data.match(new RegExp(base, 'g')) || []).length
