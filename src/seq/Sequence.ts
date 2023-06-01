a28f3600202d5bb52763b1e0a6b2f9beddd488b1
Daniel Famiyeh <daniel@podaris.com>
2023-06-01 23:34:52 +0100

    Add DNA and Sequence test

--- src/seq/Sequence.ts
+++ src/seq/Sequence.ts
@@ -4,9 +4,17 @@ export class Sequence {
   /**
    * Creates a new generalised Sequence object used to represent a variety of biological data
    *
-   * @param {string} data Biological data string
-   * @param {SequenceType} type Sequence type
+   * @param {string} _data Biological data string
+   * @param {SequenceType} _type Sequence type
    */
 
-  constructor(protected data: string, protected type: SequenceType) {}
+  constructor(protected _data: string, protected _type: SequenceType) {}
+
+  get data() {
+    return this._data;
+  }
+
+  get type() {
+    return this._type;
+  }
 }
---
+++ tests/seq/Sequence.test.ts
@@ -0,0 +1,14 @@
+import { assert } from 'chai';
+import { Sequence } from '../../src/seq/Sequence';
+
+describe('Sequence test suite', () => {
+  it('Should create a new Sequence', () => {
+    const data = 'ATCG';
+    const type = 'dna';
+
+    const seq = new Sequence(data, type);
+
+    assert.equal(seq.data, data);
+    assert.equal(seq.type, type);
+  });
+});
